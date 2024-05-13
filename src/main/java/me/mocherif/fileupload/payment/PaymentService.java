908898b6367ac212048f2bdca1d7ef598668e277
mo-cherif <mocherifh0@gmail.com>
2024-05-13 12:23:31 +0100

    feat: payment service

---
+++ src/main/java/me/mocherif/fileupload/payment/PaymentService.java
@@ -0,0 +1,70 @@
+package me.mocherif.fileupload.payment;
+
+import lombok.AllArgsConstructor;
+import me.mocherif.fileupload.student.Student;
+import me.mocherif.fileupload.student.StudentRepos;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@AllArgsConstructor
+public class PaymentService {
+    public PaymentRepos paymentRepos;
+    public StudentRepos studentRepos;
+
+    @GetMapping("/payments")
+    public List<Payment> allPayment() {
+        return paymentRepos.findAll();
+    }
+
+    @GetMapping("/payments/{id}")
+    private Payment findById(@PathVariable Long id) {
+        return paymentRepos.findById(id).get();
+    }
+
+    public List<Payment> findByStudentCode(@PathVariable String code) {
+        return paymentRepos.findByStudentCode(code);
+    }
+
+    public Payment savePayment(@RequestParam MultipartFile file, LocalDate date,
+                               double amount, PaymentType type, String studentCode) throws IOException {
+
+        var path = Paths.get(System.getProperty("user.home"), "student-app-files", "payments");
+        if(!Files.exists(path)) {
+            Files.createDirectories(path);
+        }
+        String fileId = UUID.randomUUID().toString();
+        Path filepath = Paths.get(System.getProperty("user.home"), "student-app-files",fileId+".pdf");
+        Student student = studentRepos.findByCode(studentCode);
+        Files.copy(file.getInputStream(), filepath);
+        Payment payment = Payment.builder()
+                .paymentType(type)
+                .amount(amount)
+                .student(student)
+                .paymentStatus(PaymentStatus.CREATED)
+                .file(filepath.toUri().toString())
+                .build();
+
+        return paymentRepos.save(payment);
+    }
+
+    @GetMapping(value = "/paymentFile/{paymentid}", produces = MediaType.APPLICATION_PDF_VALUE)
+    public byte [] getPaymentFile(@PathVariable Long paymentid) throws IOException {
+        Payment payment = paymentRepos.findById(paymentid).get();
+        String filePath = payment.getFile();
+        return Files.readAllBytes(Path.of(URI.create(filePath)));
+
+    }
+
+}
